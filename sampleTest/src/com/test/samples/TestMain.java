d16452a8b7b5a8652ec409377a4d0dba1cea1fa3
muralikmrms <muralikmr.ms@gmail.com>
2024-03-25 15:09:22 -0500

    first commit

---
+++ sampleTest/src/com/test/samples/TestMain.java
@@ -0,0 +1,11 @@
+package com.test.samples;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.println("hello world.");
+	}
+
+}
